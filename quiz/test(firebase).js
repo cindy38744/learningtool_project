de18ef91e7847719ee47dd44b0c3e80b5ff2ad73
Chih Yun Huang <huangzhiyun@Chih-Yuns-MBP.local>
2020-08-14 12:02:02 +0800

    fb

--- quiz/test(firebase).js
+++ quiz/test(firebase).js
@@ -7,35 +7,50 @@
   
   // Initialize your Firebase app
   firebase.initializeApp(config);
-  
-  // Reference to the recommendations object in your Firebase database
-  var recommendations = firebase.database().ref("recommendations");
-  
-  // Save a new recommendation to the database, using the input in the form
-  var submitRecommendation = function () {
-  
-    // Get input values from each of the form elements
-    var title = $("#talkTitle").val();
-    var presenter = $("#talkPresenter").val();
-    var link = $("#talkLink").val();
-  
-    // Push a new recommendation to the database using those values
-    recommendations.push({
-      "title": title,
-      "presenter": presenter,
-      "link": link
-    });
-  };
-  
-  // When the window is fully loaded, call this function.
-  // Note: because we are attaching an event listener to a particular HTML element
-  // in this function, we can't do that until the HTML element in question has
-  // been loaded. Otherwise, we're attaching our listener to nothing, and no code
-  // will run when the submit button is clicked.
-  $(window).load(function () {
-  
-    // Find the HTML element with the id recommendationForm, and when the submit
-    // event is triggered on that element, call submitRecommendation.
-    $("#recommendationForm").submit(submitRecommendation);
-  
+  //Reference for form collection(3)
+let formMessage = firebase.database().ref('register');
+
+//listen for submit event//(1)
+document
+  .getElementById('registrationform')
+  .addEventListener('submit', formSubmit);
+
+//Submit form(1.2)
+function formSubmit(e) {
+  e.preventDefault();
+  // Get Values from the DOM
+  let name = document.querySelector('#name').value;
+  let email = document.querySelector('#email').value;
+  let password = document.querySelector('#password').value;
+  let bio = document.querySelector('#bio').value;
+  let job = document.querySelector('#job').value;
+  let interest = document.querySelector('#interest').value;
+
+  //send message values
+  sendMessage(name, email, password, bio, job, interest);
+
+  //Show Alert Message(5)
+  document.querySelector('.alert').style.display = 'block';
+
+  //Hide Alert Message After Seven Seconds(6)
+  setTimeout(function() {
+    document.querySelector('.alert').style.display = 'none';
+  }, 7000);
+
+  //Form Reset After Submission(7)
+  document.getElementById('registrationform').reset();
+}
+
+//Send Message to Firebase(4)
+
+function sendMessage(name, email, password, bio, job, interest) {
+  let newFormMessage = formMessage.push();
+  newFormMessage.set({
+    name: name,
+    email: email,
+    password: password,
+    bio: bio,
+    job: job,
+    Interest: interest
   });
+}
