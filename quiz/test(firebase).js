f857f4091d2b954830304a97cfeace251a2130ab
Chih Yun Huang <huangzhiyun@Chih-Yuns-MBP.local>
2020-08-14 14:04:08 +0800

    4

--- quiz/test(firebase).js
+++ quiz/test(firebase).js
@@ -1,4 +1,11 @@
-var firebaseConfig = {
+var chatDatabase;
+$(init);
+
+
+function init() {
+  
+  // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyBZBuJRqZgqhMmf2r8kDh-bbzSqayGZA6g",
     authDomain: "best-learning-tool.firebaseapp.com",
     databaseURL: "https://best-learning-tool.firebaseio.com",
@@ -8,31 +15,40 @@ var firebaseConfig = {
     appId: "1:903760527283:web:43583df24861068f4cd5c6",
     measurementId: "G-8TG7V959DN"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  firebase.initializeApp(config);
 
-var myDataRef = firebase.database().ref();
+  var chatDatabase = firebase.database().ref("chat");
+  
+  // When the user presses enter on the message input, write the message to firebase.
+  $("#messageInput").keypress(function (e) {
+    
+    var name = $("#nameInput").val();
+    var msg = $("#messageInput").val();
+    
+    if (e.keyCode == 13 && name != "" && msg != "") {
+      
+      chatDatabase.push( {name:name, message:msg} );
+      $("#messageInput").val("");
+      
+    }
+    
+  });
 
-$('#messageInput').keypress(function(e) {
-  if (e.keyCode == 13) {
-    var name = $('#nameInput').val();
-    var text = $('#messageInput').val();
-    myDataRef.push({
-      name: name,
-      text: text
-    });
-    $('#nameInput').val('');
-    $('#messageInput').val('');
-  }
-});
+  // Add a callback that is triggered for each chat message.
+  chatDatabase.on("child_added", function (newData) {
+    
+    console.log( newData ); //not much to see here, compressed information
+    var message = newData.val();
+    
+    $("<li>")
+      .addClass("message")
+      .text(message.message)
+      .prepend($("<em>")
+      .text(message.name + ": "))
+      .appendTo($("#messages"));
+    
+    $("#messages")[0].scrollTop = $("#messagesDiv")[0].scrollHeight;
+  });
+   
+}
 
-myDataRef.on('child_added', function(snapshot) {
-  var message = snapshot.val();
-  displayChatMessage(message.name, message.text);
-});
-
-function displayChatMessage(name, text) {
-  $('<div/>').text(text).prepend($('<b/>').text(name + ': ')).appendTo($('#messagesDiv'));
-  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-};
