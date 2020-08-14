45d71484d7a6928bf4fef3c0b66d4900fda7be4c
Chih Yun Huang <huangzhiyun@Chih-Yuns-MBP.local>
2020-08-14 13:56:40 +0800

    new

--- quiz/test(firebase).js
+++ quiz/test(firebase).js
@@ -1,3 +1,17 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBZBuJRqZgqhMmf2r8kDh-bbzSqayGZA6g",
+    authDomain: "best-learning-tool.firebaseapp.com",
+    databaseURL: "https://best-learning-tool.firebaseio.com",
+    projectId: "best-learning-tool",
+    storageBucket: "best-learning-tool.appspot.com",
+    messagingSenderId: "903760527283",
+    appId: "1:903760527283:web:43583df24861068f4cd5c6",
+    measurementId: "G-8TG7V959DN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 var myDataRef = new Firebase('https://best-learning-tool.firebaseio.com');
 
 $('#messageInput').keypress(function(e) {
