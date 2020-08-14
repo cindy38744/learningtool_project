0362e0054f6217910d9bb810f1c60e4ea2ee8359
Chih Yun Huang <huangzhiyun@Chih-Yuns-MBP.local>
2020-08-14 12:14:41 +0800

    2

--- quiz/test(firebase).js
+++ quiz/test(firebase).js
@@ -1,26 +1,31 @@
-  var config = {
-    apiKey: "AIzaSyD6ib-N_S2u0mXIcw2l5QpI9gqB3zpHnZ0",
-    authDomain: "how-to-cook-rice.firebaseapp.com",
-    databaseURL: "https://how-to-cook-rice.firebaseio.com",
-    storageBucket: "how-to-cook-rice.appspot.com",
+(function(){
+    var newscript = document.createElement('script');
+       newscript.type = 'text/javascript';
+       newscript.async = true;
+       newscript.src = 'https://www.gstatic.com/firebasejs/3.0.2/firebase.js';
+    (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+  })();
+  
+  _setFormData = function setFormData (sel, data) {
+    console.info('setting form to data', data);
+    var inputList = document.querySelectorAll(sel + ' [name]');
+    [].forEach.call(inputList, function(input) {
+        console.log(input);
+        if (data[input.name] && data[input.name] !== "undefined") {
+          input.value = data[input.name];
+        }
+    });
   };
-  
-  // Initialize your Firebase app
-  firebase.initializeApp(config);
-
-  // Listen to the form submit event
-$('#myForm').submit(function(evt) {
-
-    // Target the form elements by their ids
-    // And build the form object like this using jQuery: 
-    var formData = {
-      "title": $('#title').val(),
-      "description": $('#description).val(),
-    }
-  
-    evt.preventDefault(); //Prevent the default form submit action
-    
-    // You have formData here and can do this:
-    firebase.initializeApp(config); //Initialize your firebase here passing your firebase account config object
-    firebase.database().ref('/formDataTree').push( formData ); // Adds the new form data to the list under formDataTree node
-  })
+  var _fb;
+  var fbToForm = function fbToForm (key, sel) {
+      var config = config || {
+        apiKey: "AIzaSyD6ib-N_S2u0mXIcw2l5QpI9gqB3zpHnZ0",
+        authDomain: "how-to-cook-rice.firebaseapp.com",
+        databaseURL: "https://how-to-cook-rice.firebaseio.com",
+        storageBucket: "how-to-cook-rice.appspot.com",
+      };
+      _fb = _fb && _fb.name === "fbToForm" ? _fb : firebase.initializeApp(config, "fbToForm");
+      _fb.database().ref('user-data/' + key).on('value', function(snapshot) {
+          _setFormData(sel, snapshot.val());
+      });
+  };
