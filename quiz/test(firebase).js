77a5c5b929c8198c1f224fa893632f41d880ae1a
Chih Yun Huang <huangzhiyun@Chih-Yuns-MBP.local>
2020-08-14 12:08:55 +0800

    new

--- quiz/test(firebase).js
+++ quiz/test(firebase).js
@@ -7,50 +7,20 @@
   
   // Initialize your Firebase app
   firebase.initializeApp(config);
-  //Reference for form collection(3)
-let formMessage = firebase.database().ref('register');
 
-//listen for submit event//(1)
-document
-  .getElementById('registrationform')
-  .addEventListener('submit', formSubmit);
+  // Listen to the form submit event
+$('#myForm').submit(function(evt) {
 
-//Submit form(1.2)
-function formSubmit(e) {
-  e.preventDefault();
-  // Get Values from the DOM
-  let name = document.querySelector('#name').value;
-  let email = document.querySelector('#email').value;
-  let password = document.querySelector('#password').value;
-  let bio = document.querySelector('#bio').value;
-  let job = document.querySelector('#job').value;
-  let interest = document.querySelector('#interest').value;
-
-  //send message values
-  sendMessage(name, email, password, bio, job, interest);
-
-  //Show Alert Message(5)
-  document.querySelector('.alert').style.display = 'block';
-
-  //Hide Alert Message After Seven Seconds(6)
-  setTimeout(function() {
-    document.querySelector('.alert').style.display = 'none';
-  }, 7000);
-
-  //Form Reset After Submission(7)
-  document.getElementById('registrationform').reset();
-}
-
-//Send Message to Firebase(4)
-
-function sendMessage(name, email, password, bio, job, interest) {
-  let newFormMessage = formMessage.push();
-  newFormMessage.set({
-    name: name,
-    email: email,
-    password: password,
-    bio: bio,
-    job: job,
-    Interest: interest
-  });
-}
+    // Target the form elements by their ids
+    // And build the form object like this using jQuery: 
+    var formData = {
+      "title": $('#title').val(),
+      "description": $('#description).val(),
+    }
+  
+    evt.preventDefault(); //Prevent the default form submit action
+    
+    // You have formData here and can do this:
+    firebase.initializeApp(config); //Initialize your firebase here passing your firebase account config object
+    firebase.database().ref('/formDataTree').push( formData ); // Adds the new form data to the list under formDataTree node
+  })
